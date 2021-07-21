060441aebdc9fd9375a148d3f9340b7240d1702e
Ryel Banfield <ryelbanfield@gmail.com>
2021-07-21 07:32:57 -0400

    Fix EVERYTHING so far

--- src/components/Home.js
+++ src/components/Home.js
@@ -56,30 +56,36 @@ const Home = () => (
     </div>
 
     <div className="my-stack">
-      <h3 className="stack-header">What do I love working with?</h3>
-      <ul className="stack love">
-        {technologies.love.map((technology) => (
-          <li className="stack-item" key={technology.name}>
-            <img className="stack-image" src={technology.src} alt={technology.name} />
-          </li>
-        ))}
-      </ul>
-      <h3 className="stack-header">What do I know how to work with?</h3>
-      <ul className="stack know">
-        {technologies.know.map((technology) => (
-          <li className="stack-item" key={technology.name}>
-            <img className="stack-image" src={technology.src} alt={technology.name} />
-          </li>
-        ))}
-      </ul>
-      <h3 className="stack-header">What am I learning to work with?</h3>
-      <ul className="stack learning">
-        {technologies.learning.map((technology) => (
-          <li className="stack-item" key={technology.name}>
-            <img className="stack-image" src={technology.src} alt={technology.name} />
-          </li>
-        ))}
-      </ul>
+      <div className="stack-div">
+        <h3 className="stack-header">What do I love working with?</h3>
+        <ul className="stack-items">
+          {technologies.love.map((technology) => (
+            <li className="stack-item" key={technology.name}>
+              <img className="stack-image" src={technology.src} alt={technology.name} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="stack-div">
+        <h3 className="stack-header">What do I know how to work with?</h3>
+        <ul className="stack-items">
+          {technologies.know.map((technology) => (
+            <li className="stack-item" key={technology.name}>
+              <img className="stack-image" src={technology.src} alt={technology.name} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="stack-div">
+        <h3 className="stack-header">What am I learning to work with?</h3>
+        <ul className="stack-items">
+          {technologies.learning.map((technology) => (
+            <li className="stack-item" key={technology.name}>
+              <img className="stack-image" src={technology.src} alt={technology.name} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   </main>
 );
